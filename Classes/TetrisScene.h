26afb991b23051a90ec454555dfb5d39acd3e250
Artur Barilo <faustkeller45@gmail.com>
2022-05-02 11:56:59 +0300

    master | cpp: new scene

---
+++ Classes/TetrisScene.h
@@ -0,0 +1,8 @@
+#ifndef COCOS_TETRIS_TETRISSCENE_H
+#define COCOS_TETRIS_TETRISSCENE_H
+
+#include "cocos2d.h"
+
+class TetrisScene : public cocos2d::Scene {};
+
+#endif // COCOS_TETRIS_TETRISSCENE_H
