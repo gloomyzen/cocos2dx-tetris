e4a4b1ac7f8f477d25c53a9487e758d42bc6ebce
Artur Barilo <faustkeller45@gmail.com>
2022-05-03 13:57:58 +0300

    master | wip, update scene

--- Classes/TetrisScene.cpp
+++ Classes/TetrisScene.cpp
@@ -2,6 +2,12 @@
 #include "json/document.h"
 #include <assert.h>
 
+const float widthSize = 10.f;
+const float heightSize = 20.f;
+const float tileSize = 10.f; //width and height of tile
+const float tileScale = 4.f;
+const float tickTime = 1.5f;
+
 void TetrisScene::initScene() {
     rapidjson::Document document;
     auto jsonStr = cocos2d::FileUtils::getInstance()->getStringFromFile("shapes.json");
@@ -17,6 +23,46 @@ void TetrisScene::initScene() {
 
     for (auto it = document.MemberBegin(); it != document.MemberEnd(); ++it) {
         assert(it->value.IsArray() && "shape must be array!");
-        auto name = it->name.GetString();
+        std::vector<shapeStruct> shape;
+        auto array = it->value.GetArray();
+        for (auto ar = array.Begin(); ar != array.End(); ++ar) {
+            assert(ar->IsArray() && "shape must be array!");
+            auto piece = ar->GetArray();
+            if (piece[0u].IsNumber() && piece[1u].IsNumber()) {
+                shape.emplace_back(piece[0u].GetInt(), piece[1u].GetInt());
+            }
+        }
+        shapes.push_back(shape);
     }
+    playGame();
+}
+
+void TetrisScene::playGame() {
+    auto delay = cocos2d::DelayTime::create(tickTime);
+    auto clb = cocos2d::CallFunc::create([this](){
+        spawn();
+        //todo add move
+        //todo add check
+    });
+    auto seq = cocos2d::Sequence::create(delay, clb, nullptr);
+    auto repeat = cocos2d::RepeatForever::create(seq);
+    this->runAction(repeat);
+}
+
+void TetrisScene::spawn() {
+    auto director = cocos2d::Director::getInstance();
+    auto sceneSize = director->getVisibleSize();
+    auto shape = shapes.front(); //todo add random shape
+    for (const auto& item : shape) {
+        auto sprite = new cocos2d::Sprite();
+        sprite->initWithFile("tile.png");
+        addChild(sprite);
+        sprite->setContentSize({tileSize, tileSize});
+        sprite->setScale(tileScale);
+        sprite->setAnchorPoint(cocos2d::Vec2::ZERO);
+        auto origSize = tileSize * tileScale;
+        sprite->setPositionY(sceneSize.height - origSize);
+        sprite->setPositionX(sceneSize.width / origSize / 2 * origSize);
+    }
+
 }
--- Classes/TetrisScene.h
+++ Classes/TetrisScene.h
@@ -2,10 +2,25 @@
 #define COCOS_TETRIS_TETRISSCENE_H
 
 #include "cocos2d.h"
+#include <vector>
+
+struct shapeStruct {
+    shapeStruct(int _x, int _y, cocos2d::Color3B _color = cocos2d::Color3B::BLUE) : x(_x), y(_y), color(_color) {}
+
+    int x = 0;
+    int y = 0;
+    cocos2d::Color3B color = cocos2d::Color3B::BLUE;
+};
 
 class TetrisScene : public cocos2d::Scene {
 public:
     void initScene();
+
+private:
+    void playGame();
+    void spawn();
+
+    std::vector<std::vector<shapeStruct>> shapes;
 };
 
 #endif// COCOS_TETRIS_TETRISSCENE_H
